f56a6740914523e2284a7fe93b8804d470df5a15
Vince Bloise <vbloise3@gmail.com>
2019-02-12 21:01:10 -0500

    Added prototype for build pipeline lambda function

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,19 @@
+import boto3
+from botocore.client import Config
+import io
+import zipfile
+import mimetypes
+
+s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+
+portfolio_bucket = s3.Bucket('portfolio.vincebloise.com')
+build_bucket = s3.Bucket('portfoliobuild.vincebloise.com')
+
+portfolio_zip = io.BytesIO()
+build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
