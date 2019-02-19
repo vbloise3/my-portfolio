53b092f8ada34a430c0d1f28ed677b86e3980876
Vince Bloise <vbloise3@gmail.com>
2019-02-18 19:46:21 -0500

    Changed build pipeline lambda function to work with IAM roles for upload

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,19 +1,25 @@
+import json
 import boto3
 from botocore.client import Config
 import io
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-portfolio_bucket = s3.Bucket('portfolio.vincebloise.com')
-build_bucket = s3.Bucket('portfoliobuild.vincebloise.com')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-portfolio_zip = io.BytesIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    portfolio_bucket = s3.Bucket('portfolio.vincebloise.com')
+    build_bucket = s3.Bucket('portfoliobuild.vincebloise.com')
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    portfolio_zip = io.BytesIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    print
+    "job done!"
+    return "hello from lambda"
