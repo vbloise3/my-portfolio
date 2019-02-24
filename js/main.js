de75112cdb11601198849b9e7a2e51152227b82a
Vince Bloise <vbloise3@gmail.com>
2019-02-24 10:48:51 -0500

    Added react components for work examples

--- js/main.js
+++ js/main.js
@@ -1,4 +1,34 @@
-// noinspection JSAnnotator
-import "react-dom"
+import React from 'react';
+import ReactDom from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log("loaded react-dom.");
+
+const myWork = [
+    {
+        'title': "Work Example #1",
+        'image': {
+            'desc': "example screenshot of a project involving code",
+            'src': "images/example1.png",
+            'comment': ""
+        }
+    },
+    {
+        'title': "Work Example #2",
+        'image': {
+            'desc': "example screenshot of a project involving chemistry",
+            'src': "images/example2.png",
+            'comment': `“Chemistry” by Surian Soosay is licensed under CC BY 2.0
+                         https://www.flickr.com/photos/ssoosay/4097410999`
+        }
+    },
+    {
+        'title': "Work Example #3",
+        'image': {
+            'desc': "example screenshot of a project involving cats",
+            'src': "images/example3.png",
+            'comment': `“Bengal cat” by roberto shabs is licensed under CC BY 2.0
+                         https://www.flickr.com/photos/37287295@N00/2540855181`
+        }
+    }
+];
+ReactDom.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
