f9ba610258460f9a0cc7f950be2c9e8d2bcb7ce0
Vince Bloise <vbloise3@gmail.com>
2019-02-24 12:33:52 -0500

    Removedfailed test

--- __tests__/test-example-work.js
+++ __tests__/test-example-work.js
@@ -49,6 +49,5 @@ describe("ExampleWorkBubble component", () => {
 
    it("Should have the image source set correctly", () => {
        expect(images.prop('src')).toEqual(myWork[1].image.src);
-       expect(5).toEqual(6);
    })
 });
